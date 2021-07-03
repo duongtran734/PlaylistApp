a65693576e321578427ad247f1d91e4f0c44cdcc
duongtran734@gmail.com <duongtran734@gmail.com>
2021-07-03 19:57:05 -0400

    Added DbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlaylistApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlaylistApp.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options ) : base(options)
+        {
+        }
+
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<ArtistSong> ArtistSongs { get; set; }
+
+    }
+}
